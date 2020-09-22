76025a4d9b96d1d3685a047d93651f09a973a964
Dhimiter Bundo <dhimiter.bundo@ikubinfo.al>
2020-09-23 01:27:40 +0200

    Add Imports

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
+import { VueFormBuilderPlugin } from 'v-form-builder'
+import 'v-form-builder/dist/v-form-builder.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueFormBuilderPlugin, {})
+Vue.use(VueAxios, axios)
