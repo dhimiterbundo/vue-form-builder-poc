876ceee8d837241a3ed434c23e32862d463f80bf
Dhimiter Bundo <dhimiter.bundo@ikubinfo.al>
2020-09-23 12:14:02 +0200

    wip doc.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,14 @@
     <h2>This is a form builder Demo </h2>
   <div>
     This demo uses <a target="_blank" href="https://github.com/typicode/json-server">JSON-Server</a> as a mock server.<br>
+    <hr>
+<strong>Dependencies</strong> of the Vue Form Builder, from the version 2.0.0 and above. Here is the list:<br>
+
+Vue JS (obviously) - But it won't be built together with the final library bundle<br><br>
+<strong>vuedraggable</strong> vuedraggable: From Sortable.JS which is very lightweight and much lighter than JQuery + JQuery UI<br>
+<strong>deep-equal</strong> : A simple method to check the Form Configuration<br>
+<strong>Datepicker</strong>  - LitePicker (Super lightweight, ~5KB): <a target="_blank" href="https://wakirin.github.io/Litepicker/">https://wakirin.github.io/Litepicker/</a><br>
+<strong>DayJS</strong>  - Parse/Formatting date (2KB): <a target="_blank" href="https://day.js.org/en/">https://day.js.org/en/</a>
 
   </div>
   </div>
